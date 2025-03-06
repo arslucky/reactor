1f2cc415088aab84e2cda779fadbd26fed609a6c
arslucky <arsibragimov@gmail.com>
2025-03-06 07:30:29 +0300

    added generate,create,push operators

--- src/test/java/org/ars/example/reactor/ReactorApplicationTests.java
+++ src/test/java/org/ars/example/reactor/ReactorApplicationTests.java
@@ -7,8 +7,13 @@ import org.reactivestreams.Subscription;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.SignalType;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Log4j2
 @SpringBootTest
@@ -109,5 +114,88 @@ class ReactorApplicationTests {
         flux
             .doOnRequest(r -> log.info("request of {}", r))
             .subscribe(new SimpleSubscriber());
+    }
+
+    //Example of state-based generate
+    @Test
+    void generate() {
+        var flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next(state);
+                    if (state >= 5) sink.complete();
+                    return state + 1;
+                },
+                (state) -> log.info("producer: {}", state)
+        );
+        flux.subscribe(s -> log.info("subscriber: {}", s));
+    }
+
+    @Test
+    void create() {
+        var flux = Flux.create(sink -> {
+            var events = new Integer[] {1,2,3,4,5};
+            Stream.of(events).forEach(sink::next);
+//            sink.complete();
+        });
+        flux.subscribe(log::info, log::error, () -> log.info("complete"));
+    }
+
+    @Test
+    void push() {
+        var flux = Flux.push(sink -> {
+            var events = new Integer[] {1,2,3,4,5};
+            Stream.of(events).forEach(sink::next);
+//            sink.complete();
+        });
+        flux.subscribe(log::info, log::error, () -> log.info("complete"));
+    }
+
+    @Test
+    void createInMultiThreading() {
+        var sequenceCreator = new SequenceCreator();
+        int numberElementsToEmit = 10_000;
+        Flux<Integer> flux = Flux.create(sharedSink -> sequenceCreator.multiThreadSource(numberElementsToEmit, sharedSink));
+        StepVerifier.create(flux)
+                .expectNextCount(numberElementsToEmit)
+                .verifyComplete();
+    }
+    /**
+    Test has to be failed as push operator is allowed for single thread processing
+     @see <a href="https://projectreactor.io/docs/core/release/api/"/>
+     @see <a href="https://stackoverflow.com/questions/58480997/what-is-the-difference-between-flux-create-vs-flux-push-in-project-reactor"/>
+     */
+    @Test
+    void pushInMultiThreading() {
+        var sequenceCreator = new SequenceCreator();
+        int numberElementsToEmit = 10_000;
+        Flux<Integer> flux = Flux.push(sharedSink -> sequenceCreator.multiThreadSource(numberElementsToEmit, sharedSink));
+        StepVerifier.create(flux)
+                .expectNextCount(numberElementsToEmit)
+                .verifyComplete();
+    }
+
+    class SequenceCreator {
+        void multiThreadSource(Integer elementsToEmit, FluxSink<Integer> sharedSink) {
+            var thread1 = new Thread(() -> emitElements(sharedSink, elementsToEmit/2), "Thread_1");
+            var thread2 = new Thread(() -> emitElements(sharedSink, elementsToEmit/2), "Thread_2");
+
+            thread1.start();
+            thread2.start();
+
+            try {
+                thread1.join();
+                thread2.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            sharedSink.complete();
+        }
+        void emitElements(FluxSink<Integer> sink, Integer count) {
+            IntStream.range(1, count + 1).forEach(n -> {
+                log.info("onNext {}", n);
+                sink.next(n);
+            });
+        }
     }
 }
